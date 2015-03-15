ec90951a069610cf9c329136d07dc81e4cc3a9e1
Jerry Dunmire <jerry@home>
2015-03-15 12:52:03 -0700

    Demonstration of functions complete
    On branch master
    new file:   include/bitmap.h
    modified:   user/testDisplay.c

--- user/testDisplay.c
+++ user/testDisplay.c
@@ -10,20 +10,7 @@
 #include <mem.h>
 #include "driver/i2c_oled.h"
 #include "user_config.h"
-
-// square, 24x24 pixels. 24 columns (x) by 3 rows (y)
-unsigned char square[] =
-{
-    0xff, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01,
-    0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x55,
-
-    0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x55,
-
-    0xff, 0x80, 0x80, 0x80, 0x00, 0x00, 0x00, 0x00, 0x80, 0x80, 0x80, 0x80,
-    0x00, 0x00, 0x00, 0x00, 0x80, 0x80, 0x80, 0x80, 0x00, 0x00, 0x00, 0x55,
-};
-
+#include "bitmap.h"
 
 //Init function 
 void ICACHE_FLASH_ATTR
@@ -34,7 +21,7 @@ user_init()
     i2c_init();
     OLED_Init();
     
-    OLED_Print(2, 0, "Test 1", 1);
+    OLED_Print(0, 0, "Test 1 - ", 1);
     for (line = 0; line < 8; line++)
     {
         os_sprintf(buffer, "Line %d", line);
@@ -43,7 +30,7 @@ user_init()
     os_delay_us(1000000);
     OLED_CLS();
 
-    OLED_Print(2, 1, "Test 2", 1);
+    OLED_Print(0, 1, "Test 2 - ", 1);
     for (line = 0; line < 4; line++)
     {
         os_sprintf(buffer, "Line %d", line);
@@ -52,18 +39,19 @@ user_init()
     os_delay_us(1000000);
     OLED_CLS();
 
-    OLED_Print(2, 0, "Test 3 - wrap", 1);
+    OLED_Print(0, 0, "Test 3 - wrap", 1);
     OLED_Print(0, 2, "123456789012345678901234567890", 1);
     OLED_Print(0, 2, "12345678901234567890", 2);
 
     os_delay_us(1000000);
+
     OLED_CLS();
 
-    OLED_Print(2, 0, "Test 4 - bitmap", 1);
-
-    OLED_DrawBMP(52, 3, 76, 6, square);
+    // Test #4, bit map
+    OLED_DrawBMP(0, 0, BITMAP_COLUMNS , BITMAP_ROWS, bitmap);
 
     os_delay_us(100000);
+
     // repeat by restarting
     system_restart();
 
